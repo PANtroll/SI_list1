f22b06112ddddf443af58b2a995e392705ed147c
artur <zieloneimperium230@o2.pl>
2023-03-19 14:03:07 +0100

    complete reading data.

--- src/main/java/Edge.java
+++ src/main/java/Edge.java
@@ -1,14 +1,99 @@
-import java.util.Calendar;
+import java.util.Date;
+import java.util.Objects;
 
 public class Edge {
 
+    private String company;
     private String line;
     private String startStop;
     private String endStop;
 
-    private Calendar StartTime;
-    private Calendar EndTime;
+    private Date departureTime;
+    private Date arrivalTime;
 
-    private int duration;
+    private long duration;
 
+    public void calculateDuration(){
+        this.duration = (arrivalTime.getTime() - departureTime.getTime())/60000;
+    }
+    public String getCompany() {
+        return company;
+    }
+
+    public void setCompany(String company) {
+        this.company = company;
+    }
+
+    public String getLine() {
+        return line;
+    }
+
+    public void setLine(String line) {
+        this.line = line;
+    }
+
+    public String getStartStop() {
+        return startStop;
+    }
+
+    public void setStartStop(String startStop) {
+        this.startStop = startStop.toUpperCase();
+    }
+
+    public String getEndStop() {
+        return endStop;
+    }
+
+    public void setEndStop(String endStop) {
+        this.endStop = endStop.toUpperCase();
+    }
+
+    public Date getDepartureTime() {
+        return departureTime;
+    }
+
+    public void setDepartureTime(Date departureTime) {
+        this.departureTime = departureTime;
+    }
+
+    public Date getArrivalTime() {
+        return arrivalTime;
+    }
+
+    public void setArrivalTime(Date arrivalTime) {
+        this.arrivalTime = arrivalTime;
+    }
+
+    public long getDuration() {
+        return duration;
+    }
+
+    public void setDuration(int duration) {
+        this.duration = duration;
+    }
+
+    @Override
+    public String toString() {
+        return "Edge{" +
+                "company='" + company + '\'' +
+                ", line='" + line + '\'' +
+                ", startStop='" + startStop + '\'' +
+                ", endStop='" + endStop + '\'' +
+                ", DepartureTime=" + departureTime +
+                ", ArrivalTime=" + arrivalTime +
+                ", duration=" + duration +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Edge edge)) return false;
+        return getDuration() == edge.getDuration() && Objects.equals(getCompany(), edge.getCompany()) && Objects.equals(getLine(), edge.getLine()) && Objects.equals(getStartStop(), edge.getStartStop()) && Objects.equals(getEndStop(), edge.getEndStop()) && Objects.equals(getDepartureTime(), edge.getDepartureTime()) && Objects.equals(getArrivalTime(), edge.getArrivalTime());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getCompany(), getLine(), getStartStop(), getEndStop(), getDepartureTime(), getArrivalTime(), getDuration());
+    }
 }
