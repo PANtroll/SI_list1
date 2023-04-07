40005eed04d70498e96915081d1543014f1dac73
artur <zieloneimperium230@o2.pl>
2023-04-07 15:59:29 +0200

    commit

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -31,6 +31,44 @@ public class App {
         return null;
     }
 
+    public void dijkstra(Date startTime, Stop start, Stop end, Map<Stop, Map<Stop, Map<String, List<Date>>>> data) {
+
+        int[] acc = new int[data.size()];
+        Stop[] parents = new Stop[data.size()];
+        boolean[] visited = new boolean[data.size()];
+        List<Stop> stopListAll = data.keySet().stream().toList();
+        Map<Stop, Map<String, List<Date>>> toCheck = data.get(start);
+        for (int i = 0; i < acc.length; i++) {
+            acc[i] = Integer.MAX_VALUE;
+            visited[i] = false;
+        }
+        stopListAll.remove(start);
+        while (!toCheck.isEmpty()) {
+            for (Stop neighbour : data.get(start).keySet()) {
+                if(neighbour.equals(end)){
+
+                }
+                int min = Integer.MAX_VALUE;
+                String minLine = "";
+                Date minDate = null;
+                visited[0] = true;
+                parents[0] = start;
+                for (String line : data.get(start).get(neighbour).keySet()) {
+                    for (Date date : data.get(start).get(neighbour).get(line)) {
+                        int distance = calculateTime(startTime, date);
+                        if (distance < min) {
+                            min = distance;
+                            minLine = line;
+                            minDate = date;
+                        }
+                    }
+                }
+            }
+
+        }
+
+
+    }
 
     public String app(Stop start, Stop end, Date time, String type, Map<Stop, Map<Stop, Map<String, List<Date>>>> data) {
 
@@ -38,10 +76,10 @@ public class App {
         int accCost = Integer.MAX_VALUE;
         int minCost = Integer.MAX_VALUE;
 
-        if(data.get(start).containsKey(end)){
-            for(String line :data.get(start).get(end).keySet()){
-                for(Date date :data.get(start).get(end).get(line)){
-                    if(date.after(time) || date.after(new Date(time.getTime()-24*60*60*1000))){
+        if (data.get(start).containsKey(end)) {
+            for (String line : data.get(start).get(end).keySet()) {
+                for (Date date : data.get(start).get(end).get(line)) {
+                    if (date.after(time) || date.after(new Date(time.getTime() - 24 * 60 * 60 * 1000))) {
                         minCost = getMinCost(date, time, minCost, result, end, line);
                     }
                 }
@@ -54,10 +92,10 @@ public class App {
 
     private int getMinCost(Date date, Date time, int minCost, List<NodeResult> result, Stop end, String line) {
         int accCost = calculateTime(date, time);
-        if(accCost < minCost){
+        if (accCost < minCost) {
             minCost = accCost;
-            if(!result.isEmpty())
-                result.remove(result.size()-1);
+            if (!result.isEmpty())
+                result.remove(result.size() - 1);
             result.add(new NodeResult(end, date, line));
         }
         return minCost;
@@ -72,7 +110,7 @@ public class App {
         for (Edge connection : listOfEdges) {
             Stop startStop = connection.getStartStop();
             Stop endStop = connection.getEndStop();
-            if(startStop.getName().equals("Bierzyce")){
+            if (startStop.getName().equals("Bierzyce")) {
                 System.out.println();
             }
             if (startStop == null) {
@@ -93,7 +131,7 @@ public class App {
         return mapOfStops;
     }
 
-    public int calculateTime(Date time1, Date time2){
-        return (int) (time1.getTime() - time2.getTime())/60000;
+    public int calculateTime(Date time1, Date time2) {
+        return (int) (time1.getTime() - time2.getTime()) / 60000;
     }
 }
